6c794f411c7da6604dd9c5591e930e4ac44c88e4
apoorvam12 <67253499+apoorvam12@users.noreply.github.com>
2022-04-13 15:34:43 -0500

    Update parser.py
    fixing linting errors

--- parser.py
+++ parser.py
@@ -12,8 +12,6 @@ def analyze_tokens(token_list):
     if token_list[0].lower() == "go":
         if len(token_list) > 1:
             pass
-            # would call a function for navigating the player here 
+            # would call a function for navigating the player here
         else:
-           print("Please provide a direction or adjacent room to go to.") 
-            
-        
+            print("Please provide a direction or adjacent room to go to.")
