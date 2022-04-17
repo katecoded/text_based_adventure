ff5481994c0e0b1e6d4f8b23861a4683939d12bd
fmtitov <36996070+fmtitov@users.noreply.github.com>
2022-04-17 18:01:46 -0500

    Version 2.0 for parser
    Second iteration of parser. Can be updated with new actions and prepositions as we work more on project. For now only splits the input into commands, possible objects and relevant prepositions. It will be up to a separate method to verify if input makes sense that I didn't want to work on till I look at how the other parts are implemented. The temp_parser is hence a placeholder for a sample of how that logic would look like.

--- parser.py
+++ parser.py
@@ -1,9 +1,91 @@
 # import string
+available_actions = ["inventory", "help", "savegame", "loadgame", "combine",
+                     "take", "grab", "get", "pick", "go", "move", "look"]
+prepositions = ["in", "at", "to", "with"]
+special_words = ["up"]
 
 
 def tokenize(input):
+    # Initially this will split string at spaces, may be refined after considering other strings
     token_list = input.split()
+    for token in token_list:
+        token = token.lower()
     return token_list
+
+
+def logic_splitter(token_list):
+    length = len(token_list)
+    if length == 0:
+        return None, None
+    action = None
+    object_preposition_list = []
+    index = 0
+    sentence_part = ()
+    if token_list[0] in available_actions:
+        action = token_list[0]
+        index = 1
+    for word in token_list[index:]:
+        if word in prepositions or word in special_words:
+            if sentence_part != ():
+                object_preposition_list.append(sentence_part)
+            sentence_part = (word,)
+            object_preposition_list.append(sentence_part)
+            sentence_part = ()
+        else:
+            sentence_part += (word,)
+    if sentence_part != ():
+        object_preposition_list.append(sentence_part)
+    return action, object_preposition_list
+
+
+def refine_input(input):
+    token_list = tokenize(input)
+    action, tuple_list = logic_splitter(token_list)
+    if tuple_list is None:
+        return "Error", ["No user input"]
+    str_list = []
+    for tuple in tuple_list:
+        str = ""
+        str += tuple[0]
+        for word in tuple[1:]:
+            str += " " + word
+        str_list.append(str)
+    if action is None:
+        action = "go?"
+    if action == "look" and str_list[0] == "at":
+        str_list.pop(0)
+        action = "lookat"
+    if action == "pick" and str_list[0] == "up":
+        str_list.pop(0)
+        action = "take"
+    return action, str_list
+
+
+def temp_parser(input):
+    action, str_list = refine_input(input)
+    if action == "inventory":
+        print("Displays inventory to user")
+    elif action == "help":
+        print("Displays list of standard actions to user")
+    elif action == "savegame":
+        print("Saves the current game state after asking for confirmation")
+    elif action == "loadgame":
+        print("Loads last game save after asking for confirmation")
+    elif action == "look":
+        print("Gives long description of room")
+    elif len(str_list) > 0 and action == "lookat":
+        print("Attempts to look at the object " + str_list[0])
+    elif len(str_list) > 0 and action == "take" or action == "grab" or action == "get":
+        print("Attempts to take the object " + str_list[0])
+    elif len(str_list) > 0 and action == "go" or action == "move":
+        print("Attempts to go in the direction " + str_list[0])
+    # Similar to above but should return different error
+    elif len(str_list) > 0 and action == "go?":
+        print("Attempts to go in the direction " + str_list[0])
+    elif len(str_list) > 2 and action == "combine" and str_list[1] == "with":
+        print("Attempts to combine " + str_list[0] + " with " + str_list[2])
+    else:
+        print("Sorry I don't understand how to do that")
 
 
 def analyze_tokens(token_list):
