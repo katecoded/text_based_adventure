09e452779e0635dfb21a05cdd1cc3d644d005386
Masonstiller <67441648+Masonstiller@users.noreply.github.com>
2022-04-19 18:06:47 -0700

    updated
    made class variables private and improved switch lock function

--- objects/door.py
+++ objects/door.py
@@ -8,47 +8,51 @@ class Door:
         Initializes door object with a name, a room, a direction, a key status, and a lock status.
         """
 
-        self.name = name
-        self.room = room
-        self.direction = direction
-        self.key = key
-        if self.key:
-            self.locked = True
+        self._name = name
+        self._room = room
+        self._direction = direction
+        self._key = key
+        if self._key:
+            self._locked = True
         else:
-            self.locked = False
-        self.short_desc = short_desc
-        self.long_desc = long_desc
+            self._locked = False
+        self._short_desc = short_desc
+        self._long_desc = long_desc
 
     def get_name(self):
         """returns the name of the door"""
-        return self.name
+        return self._name
 
     def get_room(self):
         """returns the name of the room the door leads to"""
-        return self.room
+        return self._room
 
     def get_direction(self):
         """returns the direction of the door in the current room the player is in"""
-        return self.direction
+        return self._direction
 
     def get_key(self):
         """returns the name of the key or item needed to progress through the door"""
-        return self.key
+        return self._key
 
     def get_lock_status(self):
         """returns whether door is locked or not. True for locked, False for unlocked."""
-        return self.locked
+        return self._locked
 
     def get_short_description(self):
         """returns short description string of the door"""
-        return self.short_desc
+        return self._short_desc
 
     def get_long_description(self):
         """returns long description string of the door"""
-        return self.long_desc
+        return self._long_desc
 
     def unlock_door(self):
         """
         switch lock status of door
         """
-        self.locked = False
+        if self._locked:
+            self._locked = False
+
+        if not self._locked:
+            self._locked = True
