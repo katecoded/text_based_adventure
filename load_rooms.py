730973d6fbb95478e54cd6532089ddb4a5bbd215
Katelyn Lindsey <katelyn.lindsey77@gmail.com>
2022-04-24 01:07:40 -0700

    Implements load_rooms function and a test room json file.

--- load_rooms.py
+++ load_rooms.py
@@ -1,35 +1,69 @@
 import json
 import os
 from objects.room import Room
+from objects.item import Item
+from objects.door import Door
+
+
+def create_object_dictionary(object_data, object_type):
+    """
+    Takes a dictionary of data for either items or doors
+    and returns a dictionary of the objects of the
+    specified type with the names as keys.
+    """
+    result_dict = {}
+
+    # for each key, create a game object of the specified type and
+    # add it to the dictionary
+    for name in object_data.keys():
+        if object_type == "item":
+            result_dict[name] = Item(name, object_data[name]["description"],
+                                     object_data[name]["takeable"])
+        else:
+            result_dict[name] = Door(name, object_data[name]["destination"],
+                                     object_data[name]["direction"],
+                                     object_data[name]["key"],
+                                     object_data[name]["short_description"],
+                                     object_data[name]["long_description"])
+
+    return result_dict
 
 
 def load_rooms():
     """
-    Finds all of the json files in the rooms folder and
-    turns them into Room objects. All of the items and doors
+    Finds all room JSON files in the rooms folder and
+    turns them into Room objects. All the items and doors
     in the file are also turned into Item objects and Door
     objects, respectively.
-    Returns a dictionary of all of the Room objects with
+    Returns a dictionary of all the Room objects with
     their names as keys.
     """
-    # empty dictionary to store Rooms in
     room_dict = {}
 
-    # 1. Get list of json files in rooms folder.
+    # get list of all JSON files in rooms folder.
     room_files = os.listdir(os.path.abspath("rooms"))
 
-    # 2. For each file:
+    # iterate through each file of room data
     for file in room_files:
-        with open(file) as room_json:
-            # 1. Turn JSON into a Python dictionary.
+
+        # get the relative path to the room data file and open it
+        file_path = "rooms/" + file
+        with open(file_path) as room_json:
+
             room_data = json.load(room_json)
 
-            # 2. Create a Room object with the given dictionary with empty
-            #   items and doors dictionaries.
+            # create Item object and Room object dictionaries
+            items = create_object_dictionary(room_data["items"], "item")
+            doors = create_object_dictionary(room_data["doors"], "door")
 
-            # 3. Turn each item into an Item object and add it to the room.
-            # 4. Turn each door into a Door object and add it to the room.
-            # 5. Add Room to room_dict.
+            # create a Room object with the Item and Door dictionaries
+            room_dict[room_data["name"]] = Room(room_data["name"],
+                                                room_data["short_description"],
+                                                room_data["long_description"],
+                                                doors, items)
 
-    # 3. Return dictionary of Rooms
     return room_dict
+
+
+if __name__ == "__main__":
+    load_rooms()
