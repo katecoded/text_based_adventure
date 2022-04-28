0a9e479e0a82b982c3dd08171f8a8f61b261aa9e
Masonstiller <67441648+Masonstiller@users.noreply.github.com>
2022-04-27 21:11:29 -0700

    updated door.py
    Updates door.py with current functions

---
+++ objects/door.py
@@ -0,0 +1,53 @@
+class Door:
+    """
+    Represents a door leading to a room in a text based adventure game.
+    """
+
+    def __init__(self, name, destination, direction, key, desc):
+        """
+        Initializes door object with a name, a room, a direction, a key status, and a lock status.
+        """
+
+        self._name = name
+        self._destination = destination
+        self._direction = direction
+        self._key = key
+        if self._key:
+            self._locked = True
+        else:
+            self._locked = False
+        self._desc = desc
+
+    def get_name(self):
+        """returns the name of the door"""
+        return self._name
+
+    def get_destination(self):
+        """returns the name of the room the door leads to"""
+        return self._destination
+
+    def get_direction(self):
+        """returns the direction of the door in the current room the player is in"""
+        return self._direction
+
+    def get_key(self):
+        """returns the name of the key or item needed to progress through the door"""
+        return self._key
+
+    def get_lock_status(self):
+        """returns whether door is locked or not. True for locked, False for unlocked."""
+        return self._locked
+
+    def get_description(self):
+        """returns description string of the door"""
+        return self._desc
+
+    def unlock_door(self):
+        """
+        switch lock status of door
+        """
+        if self._locked:
+            self._locked = False
+
+        if not self._locked:
+            self._locked = True
