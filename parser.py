65344d648194235cac0840adc52d8de17ccc1aec
fmtitov <36996070+fmtitov@users.noreply.github.com>
2022-05-01 20:10:33 -0500

    Update parser.py
    Updates prepositions for all currently used ones

--- parser.py
+++ parser.py
@@ -14,7 +14,8 @@ all_available_actions = non_interactive_actions + pickup_actions + \
                         movement_actions + eat_actions + drop_actions + \
                         use_actions + open_actions + \
                         examine_actions + other_actions
-prepositions = ["in", "at", "to", "with", "toward", "towards", "on", "into",
+prepositions = ["on", "upon", "at", "to", "with", "using"]
+# prepositions = ["in", "at", "to", "with", "toward", "towards", "on", "into",
                 "onto"]
 # directions = ["north", "south", "east", "west"]  # up/down?
 
