36f22d468d5244e16b9f1b40dd188de68d175a1a
apoorvam12 <67253499+apoorvam12@users.noreply.github.com>
2022-05-29 15:45:07 -0500

    Update tba_parser.py
    Corrected the error message handling for talk handler

--- tba_parser.py
+++ tba_parser.py
@@ -273,7 +273,7 @@ def examine_handler(gamestate, obj_name):
                 item = gamestate.get_inventory()[obj_name]
                 return item.get_description()
     # if the object is not found in the room or inventory
-    return "That item isn't here"
+    return "There is no item with that name here"
 
 
 def inventory_handler(gamestate, action, obj_name):
@@ -525,14 +525,19 @@ def talk_handler(gamestate, creature_name):
             while loop:
                 choice = input("> ")
                 if choice == "1":
-                    print("You: 'Let me guess, you want me to find something for you?'")
+                    print("You: 'Let me guess, you want me to find something for you too?'")
                     print("Giant Mushroom: 'No, I don't need anything. Just wanted to say hi. Have a nice day!'")
                     loop = False
-                if choice == "2":
+                elif choice == "2":
                     print("You: ''You can talk!?'")
                     print("Giant Mushroom: 'Yep. It's always such a surprise to people when they find out. "
                           "Anyway, I hope you have a great day!'")
                     loop = False
+                else:
+                  print("Not a valid choice. Try again.")
+        else:
+            # if it is an item in the room, but can't be spoken to
+            return "You can't talk to the " + creature_name
 
         return ""
     else:
@@ -542,9 +547,6 @@ def talk_handler(gamestate, creature_name):
 
         # if it is an item that is not in the room or inventory
         return "There is no creature by that name here"
-
-    # if it is an item in the room, but can't be spoken to
-    return "You can't talk to the " + creature_name
 
 
 def reveal_hidden(object_name, gamestate):
